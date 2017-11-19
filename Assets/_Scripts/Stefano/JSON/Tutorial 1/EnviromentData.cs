9904916dc3e5ec9728d2a38da612366d9d5d7371
Stefano Mauri <stefano.mauri@developersinerba.com>
2017-11-19 12:13:00 +0100

    Update my save system

---
+++ Assets/_Scripts/Stefano/JSON/Tutorial 1/EnviromentData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnviromentData: MonoBehaviour {
+	
+	[Header("Posizione elemento")]
+	public Vector3 position;
+	[Header("Oggetto distrutto?")]
+	public bool isDestroyed;
+
+
+}
---
+++ Assets/_Scripts/Stefano/JSON/Tutorial 1/SaveLoadEnviroment.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SaveLoadEnviroment : MonoBehaviour {
+
+	[Header("Oggetti da salvare nella scena")]
+	public List<EnviromentData> EnviromentObjects;
+	private string FilePath;
+
+	void Awake()
+	{
+
+		//Path del file di salvataggio
+		FilePath = Path.Combine(Application.dataPath, "EnviromentSave.json");
+
+		//Controllo se il file esiste
+		if (File.Exists (FilePath) == false) 
+		{
+
+			//Il file non esiste allora lo creo
+
+			Debug.Log ("Il file di salvataggio non esiste, lo creo");
+			File.Create (FilePath);
+
+		} 
+		else 
+		{
+			//Il file di salvataggio esiste
+
+			Debug.Log ("File salvtaggio OK");
+
+		}
+
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		Load ();
+
+	}
+
+	void Update()
+	{
+
+		if (Input.GetKey (KeyCode.S)) {
+
+			Save ();
+
+			Debug.Log ("Salvo!");
+
+		}
+
+		if (Input.GetKey (KeyCode.L)) {
+
+			Load ();
+
+			Debug.Log ("Carico i dati");
+
+		}
+
+	}
+
+	//Metodo che permette il salvataggio dei dati dell'enviroment
+	void Save()
+	{
+
+		string jsonString = JsonUtility.ToJson (EnviromentObjects[0]);
+		File.WriteAllText (FilePath, jsonString);
+
+	}
+
+	//Metodo che permette il salvataggio dei dati dell'enviroment
+	void Load()
+	{
+
+		string jsonString = File.ReadAllText (FilePath);
+		JsonUtility.FromJsonOverwrite (jsonString, EnviromentObjects);
+
+	}
+}

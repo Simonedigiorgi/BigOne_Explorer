ed9cb0a2d28b51116df48f4b1fe001633d549a30
Vincenzo Villani <v.villani84@alice.it>
2017-11-27 13:29:28 +0100

    Prove varie

--- Assets/_Scripts/Vincenzo/ProvaFiglio.cs
+++ Assets/_Scripts/Vincenzo/ProvaFiglio.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ProvaFiglio : ProvaDotween
 {
+
+    public string[] scenes;
+
     void Update()
     {
 
         if (Input.GetKeyDown(KeyCode.A))
         {
+            scenes = ReadNames();
 
-            StartCoroutine(Prova());
+            foreach(string s in scenes)
+            {
+                print(s);
+            }
+            
+            //StartCoroutine(Prova());
         }
 
 
+    }
+
+    
+
+    private static string[] ReadNames()
+    {
+        List<string> temp = new List<string>();
+        foreach (UnityEditor.EditorBuildSettingsScene S in UnityEditor.EditorBuildSettings.scenes)
+        {
+            if (S.enabled)
+            {
+                string name = S.path.Substring(S.path.LastIndexOf('/') + 1);
+                name = name.Substring(0, name.Length - 6);
+                temp.Add(name);
+            }
+        }
+        return temp.ToArray();
     }
 
     protected override IEnumerator Prova()

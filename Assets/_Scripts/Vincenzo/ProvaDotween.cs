9ab25be16d0c55c89f15842e827b6f9cd29b31a8
Vincenzo Villani <v.villani84@alice.it>
2017-11-27 12:32:30 +0100

    Added vincenzo's scene

---
+++ Assets/_Scripts/Vincenzo/ProvaDotween.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ProvaDotween : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	
+
+    protected virtual IEnumerator Prova()
+    {
+        Sequence mySequence = DOTween.Sequence();
+
+        mySequence.Append(gameObject.transform.DOMoveX(50, 2));
+        mySequence.Append(gameObject.transform.DOScaleY(10, 2));
+
+        yield return mySequence.WaitForCompletion();
+
+        Debug.Log("Ciao");
+    }
+}
---
+++ Assets/_Scripts/Vincenzo/ProvaFiglio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ProvaFiglio : ProvaDotween
+{
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+            StartCoroutine(Prova());
+        }
+
+
+    }
+
+    protected override IEnumerator Prova()
+    {
+        
+
+        yield return StartCoroutine(base.Prova());
+
+        Debug.Log("Olè");
+
+        yield return null;
+
+    }
+}

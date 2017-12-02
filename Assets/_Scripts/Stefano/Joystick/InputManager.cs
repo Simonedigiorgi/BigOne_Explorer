47b74bea40b6a6c2e067cd76ac04af6dc2f1a723
Stefano Mauri <stefano.mauri@developersinerba.com>
2017-12-02 23:00:32 +0100

    Add
    - Setting Joystick
    - Update my scene to test SettingJoystick
    
    - System to move on menu
    - Update my scene to move on menu

---
+++ Assets/_Scripts/Stefano/Joystick/InputManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class InputManager
+{
+
+	public static float MainHorizontal()
+	{
+
+		float r = 0.0f;
+		r += Input.GetAxis ("LeftAnalogHorizontal");
+		r += Input.GetAxis ("RightAnalogHorizontal");
+
+		return Mathf.Clamp (r, -1.0f, 1.0f); 
+
+	}
+
+	public static float MainVertical()
+	{
+
+		float r = 0.0f;
+		r += Input.GetAxis ("LeftAnalogVertical");
+		r += Input.GetAxis ("RightAnalogVertical");
+
+		return Mathf.Clamp (r, -1.0f, 1.0f); 
+
+  
+	}
+
+	public static Vector3 MainJoystick()
+	{
+
+		return new Vector3 (MainHorizontal (), 0, MainVertical ());
+
+	}
+
+	//----------- Buttons
+
+	public static bool ABotton()
+	{
+
+		return Input.GetButtonDown ("A");
+
+	}
+
+	public static bool BBotton()
+	{
+
+		return Input.GetButtonDown ("B");
+
+	}
+
+	public static bool XBotton()
+	{
+
+		return Input.GetButtonDown ("X");
+
+	}
+
+	public static bool YBotton()
+	{
+
+		return Input.GetButtonDown ("Y");
+
+	}
+	
+}

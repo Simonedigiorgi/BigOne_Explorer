7a983659795024c7334b5abfda39c8963d784fa2
Stefano Mauri <stefano.mauri@developersinerba.com>
2017-12-09 20:15:17 +0100

    Add script to rotate compass to specific target

---
+++ Assets/_Scripts/Stefano/HUD/CompassLocation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompassLocation : MonoBehaviour {
+
+	public Transform target;
+	public float speed;
+
+	void Update() 
+	{
+		Vector3 targetDir = target.position - transform.position;
+		float step = speed * Time.deltaTime;
+		Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+		transform.rotation = Quaternion.LookRotation(newDir);
+	}
+
+	//Metodo per ruotare il compasso 
+	public void Compass()
+	{
+
+
+
+	}
+
+}

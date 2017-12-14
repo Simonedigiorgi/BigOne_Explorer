00cf7704fc8b3d7026796e054a78d7a55e6ecaa9
Stefano Mauri <dbga_42@dbga.local>
2017-12-14 14:40:33 +0100

    Add main menu

---
+++ Assets/_Scripts/Stefano/Menu/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour 
+{
+
+	/// <summary>
+	/// Metodo per uscire dal gioco
+	/// </summary>
+	public void Exit()
+	{
+
+		Application.Quit ();
+
+	}
+
+	//Metodo per caricare i dati
+	public void Load()
+	{
+
+		//Metodo
+
+	}
+
+
+
+}

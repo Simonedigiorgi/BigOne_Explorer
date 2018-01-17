b56af287325a4c196e6f15a8bfc651240d8e53aa
DBGA\dbga_62 <giorgio.bogni@live.it>
2018-01-17 16:29:53 +0100

    Dynamic Lighting: Rotating sun, color changing skybox; (only in _Main_Giorgio);

---
+++ Assets/_Scripts/Giorgio/DayNightCycle.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class DayNightCycle : MonoBehaviour {
+
+    public float time;
+    public TimeSpan currentTime;
+    public Transform SunTransform;
+    public Light Sun;
+    public Text timeText;
+    public int Days;
+
+    public float intensity;
+    public Color fogDay = Color.grey;
+    public Color fogNight = Color.black;
+
+ 
+
+    public int Speed;
+
+	void Update () {
+        ChangeTime();
+	}
+
+    public void ChangeTime()
+    {
+        time += Time.deltaTime + Speed;
+        if (time > 8)
+        {
+            Days += 1;
+            time = 0;
+        }
+        currentTime = TimeSpan.FromSeconds(time);
+
+        SunTransform.rotation = Quaternion.Euler(new Vector3((time-2)/8*360,0,0));
+
+        if (time < 432)
+            intensity = 1 - (4 - time) / 4;
+        else
+            intensity = 1 - ((4 - time) / 4 * -1);
+
+        RenderSettings.fogColor = Color.Lerp(fogNight, fogDay, intensity * intensity);
+
+        Sun.intensity = intensity;
+    }
+
+
+}

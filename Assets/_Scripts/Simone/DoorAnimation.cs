6121af6cd8b9d238736ac6e0ce0f58b59a22439b
Simone Di Giorgi <simonetomi@gmail.com>
2018-02-12 17:14:02 +0100

    Porta animata (prefab "BaseDoor")

---
+++ Assets/_Scripts/Simone/DoorAnimation.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimation : MonoBehaviour {
+
+    private AudioSource source;
+
+    public AudioClip open;
+    public AudioClip close;
+
+    private Animator anim;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player") // + AGGIUNGERE CONDIZIONE EQUIPAGGIAMENTO
+        {
+            //source.PlayOneShot(open); // TOGLIERE IL COMMENTO
+
+            anim.SetBool("LeftOpen", true);
+            anim.SetBool("RightOpen", true);
+
+            anim.SetBool("LeftClose", false);
+            anim.SetBool("RightClose", false);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player") // + AGGIUNGERE CONDIZIONE EQUIPAGGIAMENTO
+        {
+            //source.PlayOneShot(close); // TOGLIERE IL COMMENTO
+
+            anim.SetBool("LeftOpen", false);
+            anim.SetBool("RightOpen", false);
+
+            anim.SetBool("LeftClose", true);
+            anim.SetBool("RightClose", true);
+
+        }
+    }
+
+}

ec10bc7f181ba893f8bcb93419fe561c3f93afb7
Simone Di Giorgi <simonetomi@gmail.com>
2018-02-12 17:55:26 +0100

    Pala (bisogna sistemare le posizioni)

---
+++ Assets/_Scripts/Simone/Shovel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shovel : MonoBehaviour {
+
+    public GameObject poketShovel;
+    public GameObject handShovel;
+
+	// Use this for initialization
+	void Start () {
+
+        poketShovel.SetActive(true);
+        handShovel.SetActive(false);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GetShovel()
+    {
+        StartCoroutine(UseShovel());
+    }
+
+    public IEnumerator UseShovel()
+    {
+        handShovel.SetActive(true);
+        poketShovel.SetActive(false);
+        yield return new WaitForSeconds(4.4f);
+        handShovel.SetActive(false);
+        poketShovel.SetActive(true);
+    }
+
+
+
+
+}

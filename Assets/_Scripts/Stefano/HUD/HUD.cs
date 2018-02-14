f6429407c09ce7e09510b8f2d135c5d3d3f783cb
Stefano Mauri <dbga_42@dbga.local>
2018-02-14 19:19:02 +0100

    Update HUD

--- Assets/_Scripts/Stefano/HUD/HUD.cs
+++ Assets/_Scripts/Stefano/HUD/HUD.cs
@@ -1,19 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Invector;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class HUD : MonoBehaviour {
 
 	#region Public 
 
 	public Animator anim;
+	public GameObject eSystem;
+	public GameObject player;
+	public GameObject firstButton;
 
 	#endregion 
+
+	#region Private
+
+	private bool semaforo = true;
+
+	#endregion
+
 
 	void Update()
 	{
 
-		//Check button start
+		if (InputManager.StartButton () == true && player.GetComponent<Invector.CharacterController.vThirdPersonController> ().isGrounded == true && semaforo == true) 
+		{
+
+			semaforo = false;
+			eSystem.SetActive (true);
+			OpenMenu ();
+			player.GetComponent<Invector.CharacterController.vThirdPersonInput> ().enabled = false;
+
+		} 
+		else if (semaforo == false && InputManager.StartButton () == true) 
+		{
+
+			semaforo = true;
+			CloseMenu ();
+			player.GetComponent<Invector.CharacterController.vThirdPersonInput> ().enabled = true;
+			eSystem.SetActive (false);
+
+		}
 
 	}
 
@@ -30,12 +60,36 @@ public class HUD : MonoBehaviour {
 	}
 
 	/// <summary>
+	/// Metodo che chiude il menu	
+	/// </summary>
+	public void CloseMenu()
+	{
+
+		anim.Play ("MoveReturn");
+
+	}
+
+	/// <summary>
 	/// Metodo che ti muovi nel menu
 	/// </summa
 	public void MoveOnMenu(string value)
 	{
 
 		anim.Play (value);
+
+	}
+
+	#endregion
+
+	#region Manager
+
+	public void ResumeGame()
+	{
+
+		CloseMenu ();
+		player.GetComponent<Invector.CharacterController.vThirdPersonInput> ().enabled = true;
+		eSystem.GetComponent<EventSystem> ().SetSelectedGameObject (firstButton);
+		eSystem.SetActive (false);
 
 	}
 

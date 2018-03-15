669542e45537e957c1de9d2f451b143243e26496
Vincenzo Villani <v.villani84@alice.it>
2018-03-15 16:45:40 +0100

    Added script to lock player and camera and to kill player

---
+++ Assets/_Scripts/Vincenzo/GenericSettings.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using Invector.CharacterController;
+
+public class GenericSettings : MonoBehaviour
+{
+    //public float damageThreshold = 3f;
+    //public float damageMultiplier = 2.85f;
+
+    vThirdPersonController vController;
+
+    /*float startYPos, endYPos;
+    bool firstCall = true;
+    bool damaged = false;*/
+
+    bool isDead;
+
+    void Update()
+    {
+        
+
+        if (vThirdPersonController.instance.animator.GetFloat("VerticalVelocity") <= vThirdPersonController.instance.ragdollVel && 
+            vThirdPersonController.instance.animator.GetFloat("GroundDistance") <= 0.1f)
+        {
+            vThirdPersonController.instance.currentHealth = 0;
+        }
+            
+    }
+
+    public void LockPlayer()
+    {
+        vThirdPersonCamera.instance.lockCamera = true;
+        vThirdPersonController.instance.GetComponent<vThirdPersonInput>().lockInput = true;
+        vThirdPersonController.instance.lockSpeed = true;
+        vThirdPersonController.instance.lockRotation = true;
+    }
+
+    public void UnlockPlayer()
+    {
+        vThirdPersonCamera.instance.lockCamera = false;
+        vThirdPersonController.instance.GetComponent<vThirdPersonInput>().lockInput = false;
+        vThirdPersonController.instance.lockSpeed = false;
+        vThirdPersonController.instance.lockRotation = false;
+    }
+}

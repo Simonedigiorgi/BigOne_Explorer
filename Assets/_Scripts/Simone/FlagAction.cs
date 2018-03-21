c650633b053213f4d95484ef8ecdd2767ee6af3f
Simone Di Giorgi <simonetomi@gmail.com>
2018-03-21 15:44:28 +0100

    Flag Animation Fixed

--- Assets/_Scripts/Simone/FlagAction.cs
+++ Assets/_Scripts/Simone/FlagAction.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class FlagAction : vTriggerGenericAction {
 
-    private GameObject closeFlag;                                                   // Prendi la Bandiera chiusa
-    private GameObject openFlag;                                                    // Prendi la Bandiera aperta
+    private GameObject flag;                                                        // BANDIERA (mixamorig:RightHand, Flag)
+    private GameObject openFlag;                                                    // Prendi la Bandiera chiusa
+    private GameObject closedFlag;                                                  // Prendi la Bandiera aperta
 
     protected override void Start()
     {
         base.Start();
 
-        closeFlag = GameObject.FindGameObjectWithTag("CloseFlag");
-        openFlag = closeFlag.transform.GetChild(1).gameObject;
+        flag = GameObject.FindGameObjectWithTag("CloseFlag");
 
-        closeFlag.transform.GetChild(0).gameObject.SetActive(false);                // Prendi il Figlio
-        closeFlag.transform.GetChild(1).gameObject.SetActive(false);                // Prendi il Figlio
+        closedFlag = flag.transform.GetChild(0).gameObject;                         // Prendi il Figlio (0)
+        openFlag = flag.transform.GetChild(1).gameObject;                           // Prendi il Figlio (1)
+
+        closedFlag.gameObject.SetActive(false);                                     
+        openFlag.gameObject.SetActive(false);                                      
 
         OnDoAction.AddListener(() => GetFlag());
     }
@@ -27,23 +30,14 @@ public class FlagAction : vTriggerGenericAction {
 
     public IEnumerator UseFlag()
     {
-        closeFlag.transform.GetChild(0).gameObject.SetActive(true);
+        closedFlag.gameObject.SetActive(true);
 
         yield return new WaitForSeconds(1f);
 
-        closeFlag.transform.GetChild(0).gameObject.SetActive(false);
-        closeFlag.transform.GetChild(1).gameObject.SetActive(true);
-        this.transform.parent.GetChild(1).transform.position = 
-            new Vector3(closeFlag.transform.GetChild(1).transform.position.x + 1.8f, 
-                        closeFlag.transform.GetChild(1).transform.position.y - 1.2f, 
-                        closeFlag.transform.GetChild(1).transform.position.z);
-        closeFlag.transform.DetachChildren();
-        
-        yield return new WaitForSeconds(1f);
+        openFlag.gameObject.SetActive(true);
+        openFlag.transform.position = new Vector3 (closedFlag.transform.position.x /*+ 1.8f*/, closedFlag.transform.position.y /*- 1.2f*/, closedFlag.transform.position.z);
 
-        openFlag.SetActive(false);
-        this.transform.parent.GetChild(1).gameObject.SetActive(true);
-
-        this.gameObject.SetActive(false);
+        closedFlag.gameObject.SetActive(false);
+        flag.transform.DetachChildren(); 
     }
 }

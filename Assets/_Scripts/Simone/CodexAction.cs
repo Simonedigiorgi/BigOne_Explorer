8cd262788c71cb77b1eae8b3337fa62569506f44
Simone Di Giorgi <simonetomi@gmail.com>
2018-03-25 18:29:08 +0200

    CodexAction script + prefab

---
+++ Assets/_Scripts/Simone/CodexAction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodexAction : vTriggerGenericAction {
+
+    public GameObject codex;
+    [SerializeField] private bool isCodex;                                                          // Il Codex è attivo?
+
+	protected override void Start () {
+
+        base.Start();
+        OnDoAction.AddListener(() => GetCodex());
+
+    }
+	
+    public void GetCodex()
+    {
+        isCodex = true;
+        codex.SetActive(true);
+    }
+}

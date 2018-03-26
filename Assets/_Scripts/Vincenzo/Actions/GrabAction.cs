7ab80ed6b6f2444405b9b70c0d50a7631fdce1f4
Simone Di Giorgi <simonetomi@gmail.com>
2018-03-26 22:46:24 +0200

    Amazing rotation effects and hyper realistic animation when you collect and equipment (All Equipments on Gale Crater has the RotateEquip script attach to them, also a new trigger collider to make the magic happen). Needs to implement the Quest system Equipment mission into RotateEquip script to make it works only if you have mission enabled. You can find a comment on RotateEquip script

--- Assets/_Scripts/Vincenzo/Actions/GrabAction.cs
+++ Assets/_Scripts/Vincenzo/Actions/GrabAction.cs
@@ -1,11 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using DG.Tweening;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GrabAction : vTriggerGenericAction
 {
-    private bool isGrabbed;
+    [HideInInspector] public bool isGrabbed;
 
     protected override void Start()
     {
@@ -13,29 +10,8 @@ public class GrabAction : vTriggerGenericAction
         OnDoAction.AddListener(() => Grab());
     }
 
-    void Update () {
-
-        if (isGrabbed)
-        {
-            // Destroy(this.transform.parent.gameObject);
-        }
-	}
-
     public void Grab()
     {
-
-        StartCoroutine(GrabCoroutine());
-    }
-
-    private IEnumerator GrabCoroutine()
-    {
         isGrabbed = true;
-        yield return new WaitForSeconds(2);                              // Tempo prima di raccogliere l'oggetto 
-        transform.parent.gameObject.transform.DOScale(0, 1);
-        yield return new WaitForSeconds(1f);
-        transform.parent.gameObject.SetActive(false);
-
-
-
     }
 }

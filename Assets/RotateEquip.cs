3e6a80017f12dca91dadb33dec5e4d6d6aacb0e4
Simone Di Giorgi <simonetomi@gmail.com>
2018-03-27 22:46:01 +0200

    Fixed OntriggerExit on Equipment boxes

--- Assets/RotateEquip.cs
+++ Assets/RotateEquip.cs
@@ -11,17 +11,11 @@ public class RotateEquip : MonoBehaviour {
 
         if (hasTrigger)
             transform.Rotate(Vector3.up * 20 * Time.deltaTime);
-
-        /*if(transform.GetChild(0).GetComponent<GrabAction>().isGrabbed == true)
-        {
-            transform.Rotate(Vector3.up * 150 * Time.deltaTime);
-            StartCoroutine(Disappear());
-        }*/
 	}
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player")) // && QUEST EQUIPAGGIAMENTO ENABLED
+        if (other.gameObject.CompareTag("Player"))
         {
             transform.DOMoveY(1f, 0.3f);
             hasTrigger = true;
@@ -30,19 +24,10 @@ public class RotateEquip : MonoBehaviour {
 
     private void OnTriggerExit(Collider other)
     {
-        /*if (other.gameObject.CompareTag("Player") && transform.GetChild(0).GetComponent<GrabAction>().isGrabbed == false) // && QUEST EQUIPAGGIAMENTO ENABLED
+        if (other.gameObject.CompareTag("Player"))
         {
             transform.DOMoveY(0.5f, 0.3f);
             hasTrigger = false;
-        }*/
-    }
-
-    public IEnumerator Disappear()
-    {
-        yield return new WaitForSeconds(2);
-        transform.DOScale(0, 0.5f);
-
-        yield return new WaitForSeconds(1);
-        Destroy(gameObject);
+        }
     }
 }

063c3be2fd6db97c27c5397b7854c83dbc6f507b
Simone Di Giorgi <simonetomi@gmail.com>
2018-03-31 00:14:29 +0200

    SManager

--- Assets/_Scripts/Simone/SManager.cs
+++ Assets/_Scripts/Simone/SManager.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.PostProcessing;
+using UnityEngine.Rendering;
 using DG.Tweening;
 
 public class SManager : MonoBehaviour {
 
     // Ho messo l'opacità dei colori dei testi a 0 solo per renderli meno invasivi
+    private PostProcessingBehaviour behaviour;
+
+    public PostProcessingProfile mid;
+    public PostProcessingProfile sunset;
 
     private Text sceneText;
     private Text temperatureText;
+    private Text dayText;
 
     private int temperatureValue;
 
     void Start()
     {
+        behaviour = FindObjectOfType<vThirdPersonCamera>().GetComponent<PostProcessingBehaviour>();
+
         Scene currentScene = SceneManager.GetActiveScene();
         string sceneName = currentScene.name;
 
@@ -23,9 +33,11 @@ public class SManager : MonoBehaviour {
 
         sceneText = transform.GetChild(0).GetComponent<Text>();
         temperatureText = transform.GetChild(1).GetComponent<Text>();
+        dayText = transform.GetChild(2).GetComponent<Text>();
 
         sceneText.DOFade(0, 0);
         temperatureText.DOFade(0, 0);
+        dayText.DOFade(0, 0);
 
         if (sceneName == "Gale Crater")
             StartCoroutine(AnimateText("Gale Crater"));
@@ -42,6 +54,17 @@ public class SManager : MonoBehaviour {
     
     public IEnumerator AnimateText(string name)
     {
+        if(temperatureValue >= -80)
+        {
+            behaviour.profile = sunset;
+            dayText.text = "Time of Day: Sunset";
+        }
+        else
+        {
+            behaviour.profile = mid;
+            dayText.text = "Time of Day: Midday";
+        }
+
         yield return new WaitForSeconds(2.5f);
         sceneText.text = name;
         sceneText.DOFade(1, 4);
@@ -50,8 +73,12 @@ public class SManager : MonoBehaviour {
         temperatureText.text = "Temperature: " + temperatureValue + "°";
         temperatureText.DOFade(1, 2);
 
+        yield return new WaitForSeconds(1);
+        dayText.DOFade(1, 2);
+
         yield return new WaitForSeconds(6);
         sceneText.DOFade(0, 4);
         temperatureText.DOFade(0, 4);
+        dayText.DOFade(0, 4);
     }
 }

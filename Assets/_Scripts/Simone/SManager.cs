c94b50b33452c9f121be8ed7b28c6c88780a78d3
Simone Di Giorgi <simonetomi@gmail.com>
2018-03-31 03:49:23 +0200

    Tomorrow...

--- Assets/_Scripts/Simone/SManager.cs
+++ Assets/_Scripts/Simone/SManager.cs
@@ -12,49 +12,127 @@ using DG.Tweening;
 public class SManager : MonoBehaviour {
 
     private PostProcessingBehaviour behaviour;
+    private GameObject quest;
 
     [BoxGroup("PostProcessing Profiles")] public PostProcessingProfile mid;
     [BoxGroup("PostProcessing Profiles")] public PostProcessingProfile sunset;
 
+    // Scena
+
     private Text sceneText;
     private Text temperatureText;
     private Text dayText;
+
+    // Sol
+
+    private Text solText;
+    private bool quest1, quest2, quest3, quest4, quest5, quest6, quest7, quest8;
+
+    // Missions
+
+    private Text missionTitleText;
+    private bool mission1;
 
     private int temperatureValue;
 
     void Start()
     {
         behaviour = FindObjectOfType<vThirdPersonCamera>().GetComponent<PostProcessingBehaviour>();
+        quest = GameObject.Find("Quests");
 
         Scene currentScene = SceneManager.GetActiveScene();
         string sceneName = currentScene.name;
 
-        temperatureValue = Random.Range(-140, 20);
+        temperatureValue = Random.Range(-60, 20);
 
-        sceneText = transform.GetChild(0).GetComponent<Text>();
-        temperatureText = transform.GetChild(1).GetComponent<Text>();
-        dayText = transform.GetChild(2).GetComponent<Text>();
+        sceneText = transform.GetChild(0).transform.GetChild(0).GetComponent<Text>();
+        temperatureText = transform.GetChild(0).transform.GetChild(1).GetComponent<Text>();
+        dayText = transform.GetChild(0).transform.GetChild(2).GetComponent<Text>();
+
+        solText = transform.GetChild(1).transform.GetChild(0).GetComponent<Text>();
+
+        missionTitleText = transform.GetChild(2).transform.GetChild(0).GetComponent<Text>();
+
+        // INFO SCENA
 
         sceneText.DOFade(0, 0);
         temperatureText.DOFade(0, 0);
         dayText.DOFade(0, 0);
 
+        solText.DOFade(0, 0);
+
+        missionTitleText.DOFade(0, 0);
+
         if (sceneName == "Gale Crater")
-            StartCoroutine(AnimateText("Gale Crater"));
+            StartCoroutine(LandInfo("Gale Crater"));
 
         else if (sceneName == "Noctis Labyrinthus")
-            StartCoroutine(AnimateText("Noctis Labyrinthus"));
+            StartCoroutine(LandInfo("Noctis Labyrinthus"));
 
         else if (sceneName == "Olympus Mons")
-            StartCoroutine(AnimateText("Olympus Mons"));
+            StartCoroutine(LandInfo("Olympus Mons"));
 
         else if (sceneName == "Valles Marineris")
-            StartCoroutine(AnimateText("Valles Marineris"));
+            StartCoroutine(LandInfo("Valles Marineris"));
     }
-    
-    public IEnumerator AnimateText(string name)
+
+    private void Update()
     {
-        if(temperatureValue >= -80)
+        // INFO MISSION
+
+        if (quest.transform.GetChild(0).transform.GetChild(2).GetComponent<TaskInteract>().currentState == Task.TaskState.ENABLED && !mission1)
+        {
+            mission1 = true;
+            StartCoroutine(MissionInfo("Clear the Solar Panels"));
+        }
+
+        #region SOL
+        if (quest.transform.GetChild(0).GetComponent<Quest>().currentState == Quest.QuestState.ENABLED && !quest1)
+        {
+            quest1 = true;
+            StartCoroutine(SolInfo("Sol 1"));
+        }
+        else if (quest.transform.GetChild(1).GetComponent<Quest>().currentState == Quest.QuestState.ENABLED && !quest2)
+        {
+            quest2 = true;
+            StartCoroutine(SolInfo("Sol 6"));
+        }
+        else if (quest.transform.GetChild(2).GetComponent<Quest>().currentState == Quest.QuestState.ENABLED && !quest3)
+        {
+            quest3 = true;
+            StartCoroutine(SolInfo("Sol 15"));
+        }
+        else if (quest.transform.GetChild(3).GetComponent<Quest>().currentState == Quest.QuestState.ENABLED && !quest4)
+        {
+            quest4 = true;
+            StartCoroutine(SolInfo("Sol 21"));
+        }
+        else if (quest.transform.GetChild(4).GetComponent<Quest>().currentState == Quest.QuestState.ENABLED && !quest5)
+        {
+            quest5 = true;
+            StartCoroutine(SolInfo("Sol 28"));
+        }
+        else if (quest.transform.GetChild(5).GetComponent<Quest>().currentState == Quest.QuestState.ENABLED && !quest6)
+        {
+            quest6 = true;
+            StartCoroutine(SolInfo("Sol 34"));
+        }
+        else if (quest.transform.GetChild(6).GetComponent<Quest>().currentState == Quest.QuestState.ENABLED && !quest7)
+        {
+            quest7 = true;
+            StartCoroutine(SolInfo("Sol 41"));
+        }
+        else if (quest.transform.GetChild(7).GetComponent<Quest>().currentState == Quest.QuestState.ENABLED && !quest8)
+        {
+            quest8 = true;
+            StartCoroutine(SolInfo("Sol 49"));
+        }
+        #endregion
+    }
+
+    public IEnumerator LandInfo(string name)
+    {
+        if(temperatureValue >= -30)
         {
             behaviour.profile = sunset;
             dayText.text = "Time of Day: Sunset";
@@ -80,5 +158,27 @@ public class SManager : MonoBehaviour {
         sceneText.DOFade(0, 4);
         temperatureText.DOFade(0, 4);
         dayText.DOFade(0, 4);
+    }
+
+    public IEnumerator MissionInfo(string missionEnabled)
+    {
+        missionTitleText.text = missionEnabled;
+
+        yield return new WaitForSeconds(2.5f);
+        missionTitleText.DOFade(1, 4);
+
+        yield return new WaitForSeconds(5);
+        missionTitleText.DOFade(0, 4);
+    }
+
+    public IEnumerator SolInfo(string solNumber)
+    {
+        solText.text = solNumber;
+
+        yield return new WaitForSeconds(8f);
+        solText.DOFade(1, 4);
+
+        yield return new WaitForSeconds(4f);
+        solText.DOFade(0, 4);
     }
 }

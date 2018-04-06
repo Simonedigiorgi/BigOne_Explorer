9f0f2f09451a83f3ccaec3a28e8b723e924a48eb
Vincenzo Villani <v.villani84@alice.it>
2018-04-06 12:52:25 +0200

    Fixed cinemachine in picture's mission

--- Assets/_Scripts/Vincenzo/CinemachineManager.cs
+++ Assets/_Scripts/Vincenzo/CinemachineManager.cs
@@ -12,22 +12,11 @@ public class CinemachineManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-
-        if(Input.GetKeyDown(KeyCode.L))
-        {
-            GameObject c = GameObject.FindGameObjectWithTag("Picture").transform.GetChild(1).gameObject;
-            StartCutscene(c);
-        }
-
 		if(cutsceneActived && playableDirector)
         {
             if (playableDirector.state == PlayState.Paused)
             {
-                playableDirector.Stop();
-                playableDirector.transform.parent.gameObject.SetActive(false);
-                cutsceneActived = false;
-                vThirdPersonController.instance.GetComponent<GenericSettings>().UnlockPlayer();
-                vThirdPersonCamera.instance.GetComponent<Cinemachine.CinemachineBrain>().enabled = false;
+                StopCutscene();
             }
         }
 	}
@@ -37,20 +26,20 @@ public class CinemachineManager : MonoBehaviour {
         if(cutscene)
         {
             vThirdPersonController.instance.GetComponent<GenericSettings>().LockPlayer();
-            vThirdPersonCamera.instance.GetComponent<Cinemachine.CinemachineBrain>().enabled = true;
             cutscene.SetActive(true);
             playableDirector = cutscene.GetComponentInChildren<PlayableDirector>(true);
             playableDirector.Play();
-            cutsceneActived = true;
-            
+            cutsceneActived = true;   
         }
-        
-
     }
 
     public void StopCutscene()
     {
-
+        playableDirector.Stop();
+        playableDirector.transform.parent.gameObject.SetActive(false);
+        cutsceneActived = false;
+        vThirdPersonController.instance.GetComponent<GenericSettings>().UnlockPlayer();
+        Destroy(playableDirector.transform.parent.parent.gameObject);
     }
 
 }

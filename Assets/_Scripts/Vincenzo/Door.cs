f4d2bdbe6e7e36691b915913d9fa28f1bfba841d
Vincenzo Villani <v.villani84@alice.it>
2018-04-09 17:09:20 +0200

    Fixed exit condition

--- Assets/_Scripts/Vincenzo/Door.cs
+++ Assets/_Scripts/Vincenzo/Door.cs
@@ -4,7 +4,12 @@ public class Door : MonoBehaviour {
 
 	private void OnTriggerEnter(Collider other)
 	{
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Player") &&
+            GameManager.instance.gadgetManager.GetGadgetByType(GadgetManager.GadgetType.HELMET).isEnabled &&
+            GameManager.instance.gadgetManager.GetGadgetByType(GadgetManager.GadgetType.BACKPACK).isEnabled &&
+            GameManager.instance.gadgetManager.GetGadgetByType(GadgetManager.GadgetType.TORCH).isEnabled &&
+            GameManager.instance.gadgetManager.GetGadgetByType(GadgetManager.GadgetType.PICKAXE).isEnabled &&
+            GameManager.instance.gadgetManager.GetGadgetByType(GadgetManager.GadgetType.COMPASS).isEnabled)
         {
             StartCoroutine(other.GetComponent<GenericSettings>().ChangePlayer());
         }
